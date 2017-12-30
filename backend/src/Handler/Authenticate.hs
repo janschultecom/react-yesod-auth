a2e145d9a9961e5c93828c649f1e8079aa439208
janschultecom <jan@janschulte.com>
2017-12-30 21:49:59 +0100

    OAuth2 param extraction

--- backend/src/Handler/Authenticate.hs
+++ backend/src/Handler/Authenticate.hs
@@ -9,9 +9,37 @@ import Data.Aeson
 import GHC.Generics
 import Web.Cookie
 import Data.Time
+import Prelude as P
+import Control.Monad.Trans.Maybe (MaybeT(..), runMaybeT)
 
-getAuthenticateR :: String -> Handler ()
+
+data Provider = Google deriving Show
+newtype Code = Code Text deriving Show
+newtype Scope = Scope Text deriving Show
+newtype State = State Text deriving Show
+
+data OAuth2 = OAuth2 Provider Code Scope State deriving Show
+
+parse :: Text -> Maybe Provider
+parse "google" = Just Google
+parse _ = Nothing
+
+extractOAuth2Params :: MonadHandler m => Text -> m (Maybe OAuth2)
+extractOAuth2Params input = runMaybeT $ do
+    provider <- MaybeT $ pure $ parse input
+    code <- MaybeT $ lookupGetParam "code"
+    scope <- MaybeT $ lookupGetParam "scope"
+    state <- MaybeT $ lookupGetParam "state"
+    return $ OAuth2 provider (Code code) (Scope scope) (State state)
+
+
+getAuthenticateR :: Text -> Handler ()
 getAuthenticateR provider = do
+    --triple <- (\a b c -> (a,b,c)) <$> lookupGetParam "code" <*> lookupGetParam "scope" <*> lookupGetParam "state"
+    --triple <- liftA3 (\a b c -> (a,b,c)) (lookupGetParam "code") (lookupGetParam "scope") (lookupGetParam "state")
+    oauth2 <- extractOAuth2Params provider
+
+    _ <- lift $ P.print oauth2
     c <- lift getCurrentTime                  -- 2009-04-21 14:25:29.5585588 UTC
     let --(y,m,d) = toGregorian $ utctDay c    -- (2009,4,21)
         expires = addDays 30 $ utctDay c
